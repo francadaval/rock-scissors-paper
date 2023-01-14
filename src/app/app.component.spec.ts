5edc86bbfc8af7878b1dae929536a15f70abce2c
Fran Cadaval <francisco.cadaval@external.wolterskluwer.com>
2023-01-14 11:56:40 +0100

    Rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rock-paper-scissor'`, () => {
+  it(`should have as title 'rock-paper-s'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rock-paper-scissor');
+    expect(app.title).toEqual('rock-paper-scissors');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rock-paper-scissor app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rock-paper-scissors app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rock-paper-scissor';
+  title = 'rock-paper-scissors';
 }
