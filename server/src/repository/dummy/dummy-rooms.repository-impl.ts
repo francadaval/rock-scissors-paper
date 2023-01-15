eeb1d7d76cae3ce3bb359f4888ffc52254ca2557
Fran Cadaval <francisco.cadaval@external.wolterskluwer.com>
2023-01-15 22:31:07 +0100

    server - DummyRoomsRepositoryImpl

---
+++ server/src/repository/dummy/dummy-rooms.repository-impl.ts
@@ -0,0 +1,29 @@
+import { Room } from "../../entities/room.entity"
+import { RoomsRepository } from "../rooms.repository";
+
+const ROOMS: {
+    [id:string]: Room
+} = {};
+
+export class DummyRoomsRepositoryImpl implements RoomsRepository {
+    public async findOneById(id: string): Promise<Room>{
+        return ROOMS[id];
+    }
+
+    public async findByUsername(username: string): Promise<Room[]>{
+        return Object.values(ROOMS).filter(room => room.users.includes(username));
+    }
+
+    public async findFreeRooms(): Promise<Room[]>{
+        return Object.values(ROOMS).filter(room => room.users.length < 2);
+    }
+
+    public async save(room: Room): Promise<void>{
+        ROOMS[room._id] = room;
+    }
+
+    public async delete(room: Room): Promise<void>{
+        delete ROOMS[room._id];
+    }
+
+}
