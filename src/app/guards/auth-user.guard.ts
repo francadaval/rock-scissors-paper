369725cc9692440dd99f72a7c17ea14ed2cd907d
Fran Cadaval <francisco.cadaval@external.wolterskluwer.com>
2023-01-16 17:23:11 +0100

    app - AuthUserGuard: redirect to login

--- src/app/guards/auth-user.guard.ts
+++ src/app/guards/auth-user.guard.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { SessionService } from "../services/session.service";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthUserGuard implements CanActivate {
-    constructor(private sessionService: SessionService) {}
+    constructor(private sessionService: SessionService, private router: Router) {}
     
     async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
-        return (await this.sessionService.$userSession) ? true : false;
+        return (await this.sessionService.$userSession) ? true : this.router.parseUrl('/login');
     }
 }
