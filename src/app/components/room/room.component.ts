4fb79cabd8a88a9af52aaeaf186d6a580f4f8043
Fran Cadaval <francisco.cadaval@external.wolterskluwer.com>
2023-01-17 20:47:56 +0100

    app - RoomComponent: init

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,9 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Room } from "src/app/entities/room.entity";
+import { RoomsService } from "src/app/services/rooms.service";
 
 @Component({
 	selector: 'app-room',
 	templateUrl: './room.component.html'
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit {
 
+	constructor(
+		private route: ActivatedRoute,
+		protected roomsService: RoomsService
+	) {}
+	
+	ngOnInit() {
+		this.route.queryParams.subscribe(params => {
+		  this._roomId = params['id'];
+		});
+	}
+
+	protected _roomId: string;
+	public get room(): Room { return this.roomsService.userRooms.find(room => room._id == this._roomId); }
 }
